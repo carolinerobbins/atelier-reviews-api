1a36ab6ca54201b2629bc45cbbdfd4182d702e19
carolinerobbins <caroline.tawney@gmail.com>
2023-07-21 14:13:08 -0700

    remove compression

--- server/index.js
+++ server/index.js
@@ -3,13 +3,11 @@ const app = express();
 const router = require("./routes/routes");
 const path = require('path');
 const cors = require('cors');
-const compression = require('compression');
 
 
 app.use(cors());;
 app.use(express.json());
 app.use(express.static(path.join(__dirname, '../dist')));
-app.use(compression());
 app.use("/reviews", router);
 
 
